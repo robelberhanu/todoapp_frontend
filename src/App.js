9d745ebd88b636da0497704eba151f8ab4ae0c1a
Robel-robel <robelberhanu89@gmail.com>
2022-10-12 19:45:18 +0200

    updated App.js

--- src/App.js
+++ src/App.js
@@ -12,16 +12,7 @@ class App extends Component {
      );
   }
 }
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-         My Hello
-         <FirstComponent></FirstComponent>
-      </div>
-     );
-  }
-}
+
  
 export default App;
 

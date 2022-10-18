a03e5fffdfea078f7d448c7ba1b3cbf106ee9c2f
Robel-robel <robelberhanu89@gmail.com>
2022-10-18 16:42:06 +0200

    components practice

---
+++ src/components/firstComponent.jsx
@@ -0,0 +1,10 @@
+class FirstComponent extends Component {
+    render() {
+      return (
+        <div className="App">
+           My Hello
+           <FirstComponent></FirstComponent>
+        </div>
+       );
+    }
+  }

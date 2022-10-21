313a6e434c19de38c251206200ec858a56abfd83
Robel-robel <robelberhanu89@gmail.com>
2022-10-21 13:49:21 +0200

    added a Counter component class

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import FirstComponent from './components/firstComponent'
+import Counter from './components/counter';
 import './App.css';
  
 class App extends Component {
@@ -8,6 +9,7 @@ class App extends Component {
     return (
       <div className="App">
         <LearningComponents></LearningComponents>
+        <Counter></Counter>
       </div>
      );
   }
